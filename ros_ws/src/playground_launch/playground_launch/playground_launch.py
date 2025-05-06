9f94f407927af18a03078b0349972d14b28289f7
lucatravellerx2 <lucatraveller.x2.lightwaves@gmail.com>
2025-05-06 13:45:27 -0400

    updated launch sequence for reproducable launches

--- ros_ws/src/playground_launch/playground_launch/playground_launch.py
+++ ros_ws/src/playground_launch/playground_launch/playground_launch.py
@@ -1,12 +1,98 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+
+# from dotenv import load_dotenv
+import os
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
+
+    # load_dotenv()
+
+    pub_version = int(os.getenv("PUBLISHER_RUN"))
+
+    launch_description = LaunchDescription()
+
+    whitelist_pub_node = Node(
             package='python_playground',
-            namespace='python_playground',
-            executable='inter_pub',
-            name='inter_pub'
-        ),
-    ])
+            executable='interface_whitelist_pub',
+            name='interface_whitelist_pub'
+        )
+    
+    whitelist_sub_node = Node(
+            package='python_playground',
+            executable='interface_whitelist_sub',
+            name='interface_whitelist_sub'
+        )
+    
+    bacon_one_pub_node = Node(
+            package='python_playground',
+            executable='b1pub',
+            name='b1pub'
+        )
+    
+    bacon_one_sub_node = Node(
+            package='python_playground',
+            executable='b1sub',
+            name='b1sub'
+        )
+    
+    bacon_two_pub_node = Node(
+            package='python_playground',
+            executable='b2pub',
+            name='b2pub'
+        )
+    
+    bacon_two_sub_node = Node(
+            package='python_playground',
+            executable='b2sub',
+            name='b2sub'
+        )
+    
+    if pub_version == 1:
+        first_node = RegisterEventHandler(
+            event_handler=OnProcessStart(
+                target_action=whitelist_pub_node,
+                on_start=[
+                    bacon_one_pub_node
+                ]
+            )
+        )
+    
+        second_node = RegisterEventHandler(
+                event_handler=OnProcessStart(
+                    target_action=bacon_one_pub_node,
+                    on_start=[
+                    bacon_two_pub_node
+                    ]
+                )
+            )
+        
+        launch_description.add_action(whitelist_pub_node)
+        launch_description.add_action(first_node)
+        launch_description.add_action(second_node)
+    else:
+        first_node = RegisterEventHandler(
+            event_handler=OnProcessStart(
+                target_action=whitelist_sub_node,
+                on_start=[
+                    bacon_one_sub_node
+                ]
+            )
+        )
+    
+        second_node = RegisterEventHandler(
+                event_handler=OnProcessStart(
+                    target_action=bacon_one_sub_node,
+                    on_start=[
+                    bacon_two_sub_node
+                    ]
+                )
+            )
+        launch_description.add_action(whitelist_sub_node)
+        launch_description.add_action(first_node)
+        launch_description.add_action(second_node)
+
+    
+    return launch_description
---
+++ ros_ws/src/python_playground/python_playground/bacon2sub.py
@@ -0,0 +1,38 @@
+from std_msgs.msg import String
+
+import rclpy
+from rclpy.node import Node
+
+class InterfaceWhiteList(Node):
+
+    def __init__(self, node_name):
+        super().__init__(node_name)
+        self.get_logger().info(f">>><<<")
+
+        # self.pub = self.create_publisher(String, "test/one", 10)
+        self.sub = self.create_subscription(String, "bacon/two", self.cb, 10)
+
+        # self.timer = self.create_timer(2.0, self.timer_cb)
+    
+    def cb(self, msg):
+        self.get_logger().info(f">> We Got Msg: BACON TWO: {msg}")
+
+    def timer_cb(self):
+        msg = String()
+        msg.data = "ass"
+
+        self.get_logger().info("Sending Message>>")
+        self.pub.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    inter_node = InterfaceWhiteList("bacontwosub")
+
+    rclpy.spin(inter_node)
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
